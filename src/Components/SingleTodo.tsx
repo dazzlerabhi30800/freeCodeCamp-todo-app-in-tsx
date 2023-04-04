ccf625fefc0eab6ea5e5d8c160363c56eb6178b8
dazzlerabhi30800 <abhisingh30800@gmail.com>
2023-04-04 12:43:40 +0530

    added edit, delete, complete functionality

--- src/Components/SingleTodo.tsx
+++ src/Components/SingleTodo.tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
+import { AiFillEdit } from "react-icons/ai";
+import { MdDone } from "react-icons/md";
+import { BsFillTrashFill } from "react-icons/bs";
 import { Todo } from "../model";
 
 type Props = {
@@ -8,9 +11,68 @@ type Props = {
 };
 
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
+  const [edit, setEdit] = useState<boolean>(false);
+  const [editTodo, setEditTodo] = useState<string>("");
+
+  const handleDone = (id: number) => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, isDone: !todo.isDone } : todo
+      )
+    );
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleEdit = (e: React.FormEvent, id: number) => {
+    e.preventDefault();
+    setTodos(
+      todos.map((todo) => (todo.id === id ? { ...todo, todo: editTodo } : todo))
+    );
+    setEdit(false);
+  };
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  useEffect(() => {
+    inputRef.current?.focus();
+  });
   return (
-    <form>
-      <span className="todos__single--text">{todo.todo}</span>
+    <form className="todos__single" onSubmit={(e) => handleEdit(e, todo.id)}>
+      {edit ? (
+        <input
+          ref={inputRef}
+          type="text"
+          value={editTodo}
+          onChange={(e) => setEditTodo(e.target.value)}
+        />
+      ) : (
+        <span
+          className={`todos__singe--text ${todo.isDone ? "completed" : ""}`}
+        >
+          {todo.todo}
+        </span>
+      )}
+      <div>
+        <span
+          className="icon"
+          onClick={() => {
+            if (!edit && !todo.isDone) {
+              setEdit(!edit);
+              setEditTodo(todo.todo);
+            }
+          }}
+        >
+          <AiFillEdit />
+        </span>
+        <span className="icon" onClick={() => handleDelete(todo.id)}>
+          <BsFillTrashFill />
+        </span>
+        <span className="icon" onClick={() => handleDone(todo.id)}>
+          <MdDone />
+        </span>
+      </div>
     </form>
   );
 };
--- src/model.ts
+++ src/model.ts
@@ -3,3 +3,15 @@ export interface Todo {
   todo: string;
   isDone: boolean;
 }
+// type Actions = {
+//
+// }
+//
+// const TodoReducer = (state:Todo[], action) => {
+//
+// }
+//
+// import {useReducer} from 'react';
+// const ReducerExample = () => {
+//   const [state, dispatch] = useReducer(TodoReducer, []);
+// }
